3a0530051a0c072ff1054c6b75fd94271eb3aa1e
Christopher Kim <christopherkim511@gmail.com>
2019-12-16 15:47:59 -0500

    Bunch of stuff

--- src/App.js
+++ src/App.js
@@ -19,17 +19,68 @@ var dataPoints =[];
 var db_connection = [];
 var address_connection = [];
 var specific_address = [];
-
+var avg_Price_storage = [];
+var queried_zipAvgPrice = {"11598":6.21, "11559":3.91, "11096":3.65};
+var json_11598 = require('./11598.json');
+var json_11559 = require('./11559.json');
+var json_11096 = require('./11096.json');
 
 export default class App extends Component {
 
 	constructor(props) {
-		super(props)
-		this.state = {address: "25 HICKORY RD, WOODMERE, NY 11598"}
+		super(props)	
+		this.state = {address: "TBD", input_address_data:[], avg_Zip: 0, compare: 0, color: "blue", percent_diff:0}
 		this.handleSelect = this.handleSelect.bind(this)
 	}	
 	handleSelect(event) {
-		this.setState({address: event.value})
+		var update_comp = 0;
+		var worth_it = 'green';
+		var pcnt = 0;
+		var zip = event.value.slice(-5);
+		if (zip == "11598") {
+			for (var i = 0; i < json_11598.data.length; i++) {
+				if (event.value == json_11598.data[i].address.oneLine && update_comp==0) {
+					console.log("hi");
+					update_comp = json_11598.data[i].assessmenthistory[0].tax.taxamt / json_11598.data[i].building.size.grosssizeadjusted;
+					update_comp = update_comp.toFixed(2);
+				}
+			}
+		} else if (zip == "11559") {
+			for (var i = 0; i < json_11559.data.length; i++) {
+				if (event.value == json_11559.data[i].address.oneLine && update_comp==0) {
+					console.log("hi");
+					update_comp = json_11559.data[i].assessmenthistory[0].tax.taxamt / json_11559.data[i].building.size.grosssizeadjusted;
+					update_comp = update_comp.toFixed(2);
+				}
+			}
+		} else {
+			for (var i = 0; i < json_11096.data.length; i++) {
+				if (event.value == json_11096.data[i].address.oneLine && update_comp==0) {
+					console.log("hi");
+					update_comp = json_11096.data[i].assessmenthistory[0].tax.taxamt / json_11096.data[i].building.size.grosssizeadjusted;
+					update_comp = update_comp.toFixed(2);
+				}
+			}
+		}
+		if (queried_zipAvgPrice[zip] > update_comp) {
+			worth_it = 'red';
+			pcnt = (((queried_zipAvgPrice[zip] - update_comp)/queried_zipAvgPrice[zip])*100).toFixed(2);
+
+		} else {
+			worth_it = 'green';
+			pcnt = (((queried_zipAvgPrice[zip] - update_comp)/queried_zipAvgPrice[zip])*-100).toFixed(2);
+		}
+		console.log(update_comp);
+		this.getDataByAddress(event.value.slice(-5),event.value);
+		this.getDataFromDb(event.value.slice(-5));
+		console.log("Stored avg Price ", avg_Price_storage);
+		console.log(queried_zipAvgPrice)
+		console.log(zip);
+		console.log(queried_zipAvgPrice[zip]);
+		this.setState({address: event.value,input_address_data:specific_address, compare: update_comp, color: worth_it, percent_diff: pcnt})
+		this.setState({avg_Zip:queried_zipAvgPrice[zip], compare: update_comp});
+		console.log("current avg price output: ", this.state.avg_Zip);
+		console.log(this.state.compare);
 	}
 	testMount(){
 		fetch('./address-store.json')
@@ -129,19 +180,27 @@ export default class App extends Component {
 				}
 				specific_address.push({
 					x: new Date(data1.data.assessmenthistory[i].tax.taxyear,temp_month),
-					y: data1.data.assessmenthistory[i].tax.taxamt
+					y: data1.data.assessmenthistory[i].tax.taxamt / data1.data.building.size.grosssizeadjusted
 				});
+				if (data1.data.assessmenthistory[i].tax.taxyear == "2017") {
+					specific_address.push({
+						x: new Date(data1.data.assessmenthistory[i].tax.taxyear,temp_month),
+						y: queried_zipAvgPrice[zipcode]
+					});
+				}
+
 			}
 		})
-		console.log('Specific Address: ', specific_address)
+		console.log('Input Address Data: ', specific_address)
 		
 
 	}; 
 
 	getDataFromDb = (zipCode) => {
 		db_connection = [];
+		avg_Price_storage = [];
 
-		fetch("http://localhost:3001/api/getPropertyTax/"+zipCode)
+		fetch("./"+zipCode + ".json")
 		.then(function(response) {
 			return response.json();
 		})
@@ -156,15 +215,20 @@ export default class App extends Component {
 			return db_connection;
 		})
 		.then(function(db_connection) {
-			calc.getAvg(db_connection);
+			var avg_Price = calc.getAvg(db_connection);
+			avg_Price_storage.push({x: "Avg", y: avg_Price});
+			return avg_Price;
 		});
 	}
 
-	render() {	
+	render() {
+		this.getAddressFromDb(11598);
+
+		//change these positionings
 		const options = {
 			theme: "light2",
 			title: {
-				text: "Price per Sq Ft of " + this.state.address
+				text: "Price per Sq Ft of: " + this.state.address
 			},
 			axisY: {
 				title: "Price in USD",
@@ -175,14 +239,12 @@ export default class App extends Component {
 				type: "line",
 				xValueFormatString: "YYYY",
 				yValueFormatString: "$#,##0.00",
-				dataPoints: dataPoints //change this to specific_address
+				dataPoints: this.state.input_address_data //specific_address //change this to specific_address
 			}]
 		}
-		//change these positionings
-		this.getAddressFromDb(11598);
-		this.getDataFromDb(11598);
-		this.getDataByAddress(this.state.address.slice(-5),this.state.address);
-
+		const styles = {
+			color: this.state.color,
+		};
 
 		return (
 
@@ -193,21 +255,25 @@ export default class App extends Component {
 
 		<ReactSearchBox
         placeholder="Placeholder"
-        value="41 Cooper Square"
+        value="Please select an address"
         onSelect={this.handleSelect}
         data={address_connection}
         callback={record => console.log(record)}
       />
 
-		    
+		
 		<CanvasJSChart options = {options} 
 			 onRef={ref => this.chart = ref}
 		/>
 		{/*You can get reference to the chart instance as shown above using onRef. 
 		This allows you to access all chart properties and methods*/}
 
+		<center>
+		<p style={styles}> Average Price per Sq Ft of Zip Code {this.state.address.slice(-5)} in 2017: {this.state.avg_Zip} </p>
+		<p style={styles}> Price per Sq Ft of {this.state.address} in 2017: {this.state.compare} </p>
+		<p style={styles}> Percent Difference: {this.state.percent_diff}% from the Average Price</p>
 
-		<p> Avg Price of Zip Code</p>
+		</center>
 
       </div>
 		);
@@ -231,7 +297,8 @@ export default class App extends Component {
 			chart.render();
 		})
 		*/
-		dataPoints = specific_address;
+		//dataPoints = specific_address;
+
 		var chart = this.chart;
 		chart.render();
 		//this.getDataByZip(11598);
--- src/effCalc.js
+++ src/effCalc.js
@@ -8,16 +8,21 @@ export function getAvg(data){
 
     //console.log(data);
     let curSq = 0;
+    let totalPrice = 0;
     let numHouses = 0;
     for(let house of data){
         let sqFt = house['values']['building']['size']['grosssizeadjusted'];
         if(sqFt==0){continue;}
         curSq+=sqFt;
         numHouses+=1;
+
+        let price = house['values']['assessmenthistory'][0]['tax']['taxamt'];
+        totalPrice+=price;
         //console.log(house['values']['building']['size']['grosssizeadjusted']);
     }
 
     let avg = curSq/numHouses;
+    let price_avg = totalPrice/numHouses/avg;
 
     res['totalSqFt'] = curSq;
     res['numHouse'] = numHouses;
@@ -26,8 +31,9 @@ export function getAvg(data){
 
     console.log('Total sqFt: ',curSq);
     console.log('Total houses: ',numHouses);
-    console.log('Avergage: ',avg);
-    return res;
+    console.log('Average: ',avg);
+    console.log('Price: ', price_avg)
+    return avg;
 
 }
 
